e1256bae83d0ed11f64b920311da2cea49afc0fa
doomzzju <dongxuanlizju@gmail.com>
2017-06-28 15:32:35 +0800

    debug

--- src/Tracking.cc
+++ src/Tracking.cc
@@ -886,19 +886,19 @@ void Tracking::Track()
             {
 		if(mpMap->hasGlobalMap())
 		{
-		    ORBmatcher matcher(0.7);
-		    vector<MapPoint*> vSearchedMapPoints;
-		    vSearchedMapPoints = vector<MapPoint*>(mCurrentFrame.N,static_cast<MapPoint*>(NULL));
-		    mCurrentFrame.ComputeBoW();
-		    int TotalNum = 0;
-		    for(int i = 0; i < vCandKFs.size(); i++)
-		    {
-			ModelKeyFrame tempMdKF = vCandKFs[i];
-			int matches = matcher.SearchByModelBoW(tempMdKF, mCurrentFrame, vSearchedMapPoints);
-			cout << "In " << tempMdKF.nId << "th model kf, " << matches << " model points searched" << endl;
-			TotalNum += matches;
-		    }
-		    cout << "Total Model Points " << TotalNum << ", with ratio " << static_cast<double>(TotalNum)/static_cast<double>(mCurrentFrame.N) << endl;
+// 		    ORBmatcher matcher(0.7);
+// 		    vector<MapPoint*> vSearchedMapPoints;
+// 		    vSearchedMapPoints = vector<MapPoint*>(mCurrentFrame.N,static_cast<MapPoint*>(NULL));
+// 		    mCurrentFrame.ComputeBoW();
+// 		    int TotalNum = 0;
+// 		    for(int i = 0; i < vCandKFs.size(); i++)
+// 		    {
+// 			ModelKeyFrame tempMdKF = vCandKFs[i];
+// 			int matches = matcher.SearchByModelBoW(tempMdKF, mCurrentFrame, vSearchedMapPoints);
+// 			cout << "In " << tempMdKF.nId << "th model kf, " << matches << " model points searched" << endl;
+// 			TotalNum += matches;
+// 		    }
+// 		    cout << "Total Model Points " << TotalNum << ", with ratio " << static_cast<double>(TotalNum)/static_cast<double>(mCurrentFrame.N) << endl;
 		}
 	      
                 // Local Mapping might have changed some MapPoints tracked in last frame
